ba9b49cc93b3da8eb1599657cfd11d630fad980d
robhuff4247 <57376018+robhuff4247@users.noreply.github.com>
2022-04-03 14:30:41 -0700

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Accounts from './Accounts';
+import './style.css';
+
+
+export default function Dashboard() {
+  return(
+    <div className="Dashboard">
+      <Accounts />
+    </div>
+  );
+};
